7076ce9f03173a2c99d03f74728500666ae6c9f2
prakhar070 <bansalprakhar.2266@gmail.com>
2020-05-19 17:48:49 +0530

    User validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+
+    #validations added
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
